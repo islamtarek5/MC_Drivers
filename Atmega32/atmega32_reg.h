e9d1263e55c784aa1a8d403a67e0f8003cd7b9bc
Islam Tarek <islamtarek0550@gmail.com>
2023-06-27 10:20:32 +0300

    Add EEPROM Registers to Atmega32 register file.

--- Atmega32/atmega32_reg.h
+++ Atmega32/atmega32_reg.h
@@ -2,7 +2,7 @@
  * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
  * @CreatedDate           : 2023-06-25 12:55:48                              *
  * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
- * @LastEditDate          : 2023-06-25 13:42:48                              *
+ * @LastEditDate          : 2023-06-27 10:22:24                              *
  * @FilePath              : atmega32_reg.h                                   *
  ****************************************************************************/
 
@@ -104,6 +104,102 @@ typedef union
  * @brief Map Stack Pointer Registers and their bits to their Memory locations.
  */
 #define SPL       ((volatile SPL_t *) SPL_ADDRESS)  
-#define SPH       ((volatile SPH_t *) SPH_ADDRESS)     
+#define SPH       ((volatile SPH_t *) SPH_ADDRESS)
 
+/**
+ * @brief EEPROM Registers and Their bits .
+ * @note EEPROM Address Registers (EEARL, EEARH). (R/W Registers except reserved bits which are read-only)
+ * @note EEARH initial value is 0x00 or 0x01.
+ * @note EEARL initial value is undefined.
+ * @note EEPROM Address registers specify the EEPROM address in 1024 bytes EEPROM Space.
+ * @note EEPROM Data Register (EEDR). (R/W Register).
+ * @note EEDR contains data to be written at or read from specific address in EEPROM.
+ * @note EEDR initial value is 0x00.
+ * @note EEPROM Control Register (EECR). (R/W Registers except reserved bits which are read-only)
+ * @note EECR initial value is 0x00 or 0x02.
+ */
+
+typedef union 
+{
+    uint8_t reg;
+    struct 
+    {
+        uint8_t EEAR0    : 1;    /* EEPROM Address Bit 0 */
+        uint8_t EEAR1    : 1;    /* EEPROM Address Bit 1 */
+        uint8_t EEAR2    : 1;    /* EEPROM Address Bit 2 */
+        uint8_t EEAR3    : 1;    /* EEPROM Address Bit 3 */
+        uint8_t EEAR4    : 1;    /* EEPROM Address Bit 4 */
+        uint8_t EEAR5    : 1;    /* EEPROM Address Bit 5 */
+        uint8_t EEAR6    : 1;    /* EEPROM Address Bit 6 */
+        uint8_t EEAR7    : 1;    /* EEPROM Address Bit 7 */
+    }bits;
+
+}EEARL_t;
+
+typedef union 
+{
+    uint8_t reg;
+    struct 
+    {
+        uint8_t EEAR8    : 1;    /* EEPROM Address Bit 8 */
+        uint8_t EEAR9    : 1;    /* EEPROM Address Bit 9 */
+        uint8_t Reserved : 6;    /* Reserved bits        */
+    }bits;
+
+}EEARH_t;
+
+typedef union 
+{
+    uint8_t reg;
+    struct 
+    {
+        uint8_t EEDR0    : 1;    /* EEPROM Data Bit 0 */
+        uint8_t EEDR1    : 1;    /* EEPROM Data Bit 1 */
+        uint8_t EEDR2    : 1;    /* EEPROM Data Bit 2 */
+        uint8_t EEDR3    : 1;    /* EEPROM Data Bit 3 */
+        uint8_t EEDR4    : 1;    /* EEPROM Data Bit 4 */
+        uint8_t EEDR5    : 1;    /* EEPROM Data Bit 5 */
+        uint8_t EEDR6    : 1;    /* EEPROM Data Bit 6 */
+        uint8_t EEDR7    : 1;    /* EEPROM Data Bit 7 */
+    }bits;
+
+}EEDR_t;
+
+typedef union 
+{
+    uint8_t reg;
+    struct 
+    {
+        uint8_t EERE     : 1;    /* EEPROM Read Enable            */
+        uint8_t EEWE     : 1;    /* EEPROM Write Enable           */
+        uint8_t EEMWE    : 1;    /* EEPROM Master Write Enable    */
+        uint8_t EERIE    : 1;    /* EEPROM Ready Interrupt Enable */
+        uint8_t Reserved : 4;    /* Reserved bits                 */
+    }bits;
+
+}EECR_t;
+
+/**
+ * @brief EEPROM Registers Structure.
+ */
+
+typedef struct 
+{
+    volatile EECR_t  EECR ;
+    volatile EEDR_t  EEDR ;
+    volatile EEARL_t EEARL;
+    volatile EEARH_t EEARH;
+}EEPROM_Reg_S;
+
+
+/**
+ * @brief EEPROM Base Address.
+ */
+#define EEPROM_ADDRESS            0x3CU
+
+/**
+ * @brief Map EEPROM Registers and their bits to their Memory locations.
+ */
+#define EEPROM       ((EEPROM_Reg_S *) EEPROM_ADDRESS)
+   
 #endif
