ca572425b352d2b3833655479e9ce35e3032095b
Islam Tarek <islamtarek0550@gmail.com>
2023-06-27 11:02:32 +0300

    Add EEPROM Configurations and their options

--- Atmega32/EEPROM/EEPROM_priv.h
+++ Atmega32/EEPROM/EEPROM_priv.h
@@ -2,12 +2,29 @@
  * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
  * @CreatedDate           : 2023-06-27 10:25:18                              *
  * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
- * @LastEditDate          : 2023-06-27 10:26:07                              *
+ * @LastEditDate          : 2023-06-27 10:59:07                              *
  * @FilePath              : EEPROM_priv.h                                    *
  ****************************************************************************/
 
 #ifndef EEPROM_EEPROM_PRIV_H_
 #define EEPROM_EEPROM_PRIV_H_
 
+/**
+ * @section Configuration Options
+ */
+
+/**
+ * @brief OS Options
+ */
+
+#define SUPER_LOOP_OS           0U
+#define TIME_TRIGGER_OS         1U
+
+/**
+ * @brief EEPROM Ready Interrupt Options
+ */
+
+#define EEPROM_INTERRUPT_DISABLE    0U
+#define EERPOM_INTERRUPT_ENABLE     1U
 
 #endif
