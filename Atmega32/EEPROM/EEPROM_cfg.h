7e59027fb2ac97f96b131d34b085d3487c4600af
Islam Tarek <islamtarek0550@gmail.com>
2023-06-27 12:35:45 +0300

    Add EEPROM Period and Boot Loader configurations

--- Atmega32/EEPROM/EEPROM_cfg.h
+++ Atmega32/EEPROM/EEPROM_cfg.h
@@ -2,7 +2,7 @@
  * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
  * @CreatedDate           : 2023-06-27 10:26:29                              *
  * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
- * @LastEditDate          : 2023-06-27 11:01:23                              *
+ * @LastEditDate          : 2023-06-27 12:35:06                              *
  * @FilePath              : EEPROM_cfg.h                                     *
  ****************************************************************************/
 
@@ -24,10 +24,11 @@
 /**
  * @brief OS Configuration
  * @note Options are SUPER_LOOP_OS or TIME_TRIGGER_OS
+ * @note in Case of TIME_TRIGGER_OS, OS_PERIOD_MS must be set by the period value in milliseconds.
  */
 
-#define OS      SUPER_LOOP_OS
-
+#define OS              SUPER_LOOP_OS
+#define EEPROM_PERIOD_MS            10U
 /**
  * @brief EEPROM Interrupt Configuration
  * @note Options are EEPROM_INTERRUPT_DISABLE or EERPOM_INTERRUPT_ENABLE
@@ -35,5 +36,11 @@
 
 #define EEPROM_INTERRUPT    EEPROM_INTERRUPT_DISABLE
 
+/**
+ * @brief BOOT LOADER Configuration
+ * @note Options are BOOT_LOADER_IS_USED or BOOT_LOADER_NOT_USED
+ */
+
+#define BOOT_LOADER_USAGE   BOOT_LOADER_NOT_USED
 
 #endif
