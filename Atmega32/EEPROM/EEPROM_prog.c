ae11de813906d865ad58ed202b6d37b06540dc43
Islam Tarek <islamtarek0550@gmail.com>
2023-06-27 11:03:19 +0300

    EEPROM APIs

--- Atmega32/EEPROM/EEPROM_prog.c
+++ Atmega32/EEPROM/EEPROM_prog.c
@@ -2,7 +2,48 @@
  * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
  * @CreatedDate           : 2023-06-27 10:24:09                              *
  * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
- * @LastEditDate          : 2023-06-27 10:24:16                              *
+ * @LastEditDate          : 2023-06-27 10:50:37                              *
  * @FilePath              : EEPROM_prog.c                                    *
  ****************************************************************************/
 
+/**
+ * @section Includes
+ */
+
+#include "../atmega32_reg.h"
+#include "EEPROM_int.h"
+
+/**
+ * @section Implementation
+ */
+
+
+void EEPROM_init(void)
+{
+
+}
+
+void EEPROM_write_byte(uint16_t address, uint8_t byte)
+{
+
+}
+
+void EEPROM_write_data(uint16_t base_address, uint8_t * data, uint16_t length)
+{
+
+}
+
+void EEPROM_read_data(uint16_t base_address, uint8_t * data, uint16_t length)
+{
+
+}
+
+void EEPROM_enable_interrupt(void)
+{
+
+}
+
+void EEPROM_disable_interrupt(void)
+{
+    
+}
