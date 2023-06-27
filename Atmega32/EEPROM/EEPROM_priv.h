2e73ef2ccc3403282b4228a29d44f698b55899c1
Islam Tarek <islamtarek0550@gmail.com>
2023-06-27 11:15:33 +0300

    Add EEPROM Address Magic Numbers

--- Atmega32/EEPROM/EEPROM_priv.h
+++ Atmega32/EEPROM/EEPROM_priv.h
@@ -2,7 +2,7 @@
  * @Author                : Islam Tarek<islamtarek0550@gmail.com>            *
  * @CreatedDate           : 2023-06-27 10:25:18                              *
  * @LastEditors           : Islam Tarek<islamtarek0550@gmail.com>            *
- * @LastEditDate          : 2023-06-27 10:59:07                              *
+ * @LastEditDate          : 2023-06-27 11:15:01                              *
  * @FilePath              : EEPROM_priv.h                                    *
  ****************************************************************************/
 
@@ -17,8 +17,8 @@
  * @brief OS Options
  */
 
-#define SUPER_LOOP_OS           0U
-#define TIME_TRIGGER_OS         1U
+#define SUPER_LOOP_OS               0U
+#define TIME_TRIGGER_OS             1U
 
 /**
  * @brief EEPROM Ready Interrupt Options
@@ -26,5 +26,16 @@
 
 #define EEPROM_INTERRUPT_DISABLE    0U
 #define EERPOM_INTERRUPT_ENABLE     1U
+
+/**
+ * @section Magic Numbers
+ */
+
+/**
+ * @brief Address Magic Numbers
+ */
+
+#define ADDRESS_LSB                 0U    
+#define ADDRESS_MSB                 8U
 
 #endif
