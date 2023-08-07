3e22c6fc4a6609ad0cc1b8d0c6e9f8fac4fa3f9f
Islam Tarek <islamtarek0550@gmail.com>
2023-08-07 22:39:09 +0300

    Add Mode Manager Module files

---
+++ Atmega32/ModeManager/MM_int.h
@@ -0,0 +1,12 @@
+/*****************************************************************************
+ * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
+ * @CreatedDate           : 2023-08-07 22:36:20                              *
+ * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
+ * @LastEditDate          : 2023-08-07 22:36:30                              *
+ * @FilePath              : MM_int.h                                         *
+ ****************************************************************************/
+
+#ifndef MM_MM_INT_H_
+#define MM_MM_INT_H_
+
+#endif
---
+++ Atmega32/ModeManager/MM_prog.c
@@ -0,0 +1,8 @@
+/*****************************************************************************
+ * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
+ * @CreatedDate           : 2023-08-07 22:37:53                              *
+ * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
+ * @LastEditDate          : 2023-08-07 22:38:00                              *
+ * @FilePath              : MM_prog.c                                        *
+ ****************************************************************************/
+
