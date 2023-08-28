10f0fbdcf8e041eb77a0a3bca21b6f6575a48102
Islam Tarek <islamtarek0550@gmail.com>
2023-08-28 11:55:52 +0300

    Add GPIO folder structure

---
+++ Atmega32/GPIO/GPIO_int.h
@@ -0,0 +1,12 @@
+/*****************************************************************************
+ * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
+ * @CreatedDate           : 2023-08-28 11:52:17                              *
+ * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
+ * @LastEditDate          : 2023-08-28 11:53:55                              *
+ * @FilePath              : GPIO_int.h                                       *
+ ****************************************************************************/
+
+#ifndef GPIO_GPIO_H_
+#define GPIO_GPIO_H_
+
+#endif
---
+++ Atmega32/GPIO/GPIO_prog.c
@@ -0,0 +1,7 @@
+/*****************************************************************************
+ * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
+ * @CreatedDate           : 2023-08-28 11:54:20                              *
+ * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
+ * @LastEditDate          : 2023-08-28 11:54:24                              *
+ * @FilePath              : GPIO_prog.c                                      *
+ ****************************************************************************/
