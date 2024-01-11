8df53ead31d8d6d8f9c8f2c484863f3f5cec8f68
Islam Tarek <islam.tarek@valeo.com>
2024-01-11 11:54:26 +0200

    Add maximum accepted value to pin functions bit private macro

--- Atmega32/Timer/timer_priv.h
+++ Atmega32/Timer/timer_priv.h
@@ -2,12 +2,16 @@
  * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
  * @CreatedDate           : 2024-01-09 17:07:50                              *
  * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
- * @LastEditDate          : 2024-01-10 10:59:55                              *
+ * @LastEditDate          : 2024-01-11 11:53:19                              *
  * @FilePath              : timer_priv.h                                     *
  ****************************************************************************/
 
 #ifndef TIMER_TIMER_PRIV_H_
 #define TIMER_TIMER_PRIV_H_
 
+/**
+ * @brief Maximum Pin function bits value.
+ */
+#define MAX_PIN_FUNCTION_ACCEPTED_VALUE 3U
 
 #endif
