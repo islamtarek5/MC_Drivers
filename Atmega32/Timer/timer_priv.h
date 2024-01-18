458ba3d9d12ae68cc028e2aaec4dac2386c042af
Islam Tarek <islam.tarek@valeo.com>
2024-01-18 12:05:02 +0200

    Update the Timer Private Values

--- Atmega32/Timer/timer_priv.h
+++ Atmega32/Timer/timer_priv.h
@@ -2,7 +2,7 @@
  * @Author                : Islam Tarek<islam.tarek@valeo.com>               *
  * @CreatedDate           : 2024-01-09 17:07:50                              *
  * @LastEditors           : Islam Tarek<islam.tarek@valeo.com>               *
- * @LastEditDate          : 2024-01-11 11:53:19                              *
+ * @LastEditDate          : 2024-01-18 10:51:14                              *
  * @FilePath              : timer_priv.h                                     *
  ****************************************************************************/
 
@@ -13,5 +13,46 @@
  * @brief Maximum Pin function bits value.
  */
 #define MAX_PIN_FUNCTION_ACCEPTED_VALUE 3U
+
+/**
+ * @brief Maximum CPU Value that works with atmega32
+ * @note This Value can be applied as external clock so clock must be set to be external with Fuse bits.
+ */
+#define MAX_ACCEPTABLE_CPU_CLK          16U
+
+/**
+ * @brief Prescaler Options.
+ */
+#define TIMER_WITH_NO_PRESCALING        1U
+#define TIMER_WITH_PRESCALER_BY_8       2U
+#define TIMER_WITH_PRESCALER_BY_64      3U
+#define TIMER_WITH_PRESCALER_BY_256     4U
+#define TIMER_WIHT_PRESCALER_BY_1024    5U
+#define TIMER_WITH_EXT_CLK_FALLING_EDGE 6U
+#define TIMER_WITH_EXT_CLK_RISING_EDGE  7U
+
+/**
+ * @brief Timers' Max count Values
+ */
+#define TIMER_0_MAX_COUNT_VALUE         0xFFU
+#define TIMER_1_MAX_COUNT_VALUE         0XFFFFU
+#define TIMER_2_MAX_COUNT_VALUE         0xFFU
+
+/**
+ * @brief Overflow values.
+ */
+#define TIMER_WITH_ZERO_OVFS            0U
+
+/**
+ * @brief Timer Number of Counts.
+ */
+#define TIMER_WITH_ZERO_COUNTS          0U
+
+/**
+ * @brief Time Flag Values.
+ */
+#define TIMER_FLAG_IS_CLEARED           0U
+#define TIMER_FLAG_IS_RAISED            1U 
+#define TIMER_CLEAR_FLAG                1U
 
 #endif
